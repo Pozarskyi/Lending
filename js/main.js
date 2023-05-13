b3ab60b12ce24a5badad5254192f3b94673cf119
Evgenii <pozar1288@gmail.com>
2023-05-13 19:41:51 +0300

    Make adaptive

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
 const swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3.2,
-    spaceBetween: 30,
+    slidesPerView: 1.04,
+    spaceBetween: 3,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
+    breakpoints: {
+      600: {
+        slidesPerView: 2.2,
+        spaceBetween: 20,
+      },
+      840: {
+        slidesPerView: 2.1,
+        spaceBetween: 20,
+      },
+      841:{
+        slidesPerView:3.2,
+        spaceBetween: 30,
+      }
+    }
   });
